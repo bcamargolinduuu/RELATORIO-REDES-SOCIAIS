d343793ed952284a6486659951506ac68d2f67c9
bcamargolinduuu <105222883+bcamargolinduuu@users.noreply.github.com>
2024-10-10 08:22:02 -0300

    f

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -1,7 +1,13 @@
-const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json"
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-async function visualizarInformacoesGlobais(){
+async function visualizarInformacoesGlobais() {
+
     const res = await fetch(url)
-    const dados =  
+
+    const dados =  await res.json()
+
+    console.log(dados);
 
 }
+
+visualizarInformacoesGlobais()
